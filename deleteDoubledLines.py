8081c215ba3c1fe320fbf44a4a30b05bca03c7dc
Alexander Kern <alexgit@alexkern.de>
2020-09-29 18:45:31 +0200

    doppelte Zeilen löschen

--- deleteDoubledLines.py
+++ deleteDoubledLines.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import difflib
 import pprint
 import re
 import sys
@@ -34,16 +35,25 @@ thing = re.compile("[^>]+>[^>]+>\s*\d+(.*)")
 def differentLetters(text1, text2):
     more = 0
     moreless = 0
+    bla = ""
     for i, t1 in enumerate(text1):
         if i + moreless < len(text2) and t1 == text2[i + moreless]:
             more += 1
-        elif i + 1 < len(text2) and t1 == text2[i + 1]:
+        elif i + 1 < len(text2) and t1 == text2[i + 1 + moreless]:
             moreless += 1
             more += 1
-        elif i - 1 < len(text2) and t1 == text2[i - 1]:
+        elif i - 1 < len(text2) and t1 == text2[i - 1 + moreless]:
             moreless -= 1
             more += 1
-
+        elif i + 2 < len(text2) and t1 == text2[i + 2 + moreless]:
+            moreless += 2
+            more += 1
+        elif i - 2 < len(text2) and t1 == text2[i - 2 + moreless]:
+            moreless -= 2
+            more += 1
+        else:
+            bla += text2[i + moreless]
+    alxp(bla)
     return more
 
 
@@ -53,8 +63,9 @@ for i, line in enumerate(textList):
     textList2[i] = line
     if i > 0:
         diffi = differentLetters(textList2[i - 1], line)
+        alxp(diffi)
     else:
         diffi = len(line)
     if diffi < 10:
         multiples |= {i}
-alxp(multiples)
+# alxp(multiples)
