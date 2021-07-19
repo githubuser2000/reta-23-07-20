92af84243f4f4d1e3774268f9c4d405c76433cf2
Alexander Kern <alexgit@alexkern.de>
2021-07-19 14:04:56 +0200

    was beim Einprogrammieren - funktioniert nicht

--- lib4tables_concat.py
+++ lib4tables_concat.py
@@ -823,6 +823,27 @@ class Concat:
             ] = self.convertFractionsToDictOfNumToPaareOfMulOfIntAndFraction(
                 self.BruecheUni
             )
+            alleFractionErgebnisseMapping: dict = {
+                (0, 0, 0): "gebrRatMulSternDictUni",
+                (0, 0, 1): "gebrRatMulSternDictUni2",
+                (0, 1, 0): "gebrRatDivSternDictUni",
+                (0, 1, 1): "gebrRatDivSternDictUni2",
+                (1, 0, 0): "gebrRatMulSternDictGal",
+                (1, 0, 1): "gebrRatMulSternDictGal2",
+                (1, 1, 0): "gebrRatDivSternDictGal",
+                (1, 1, 1): "gebrRatDivSternDictGal2",
+            }
+            alleFractionErgebnisseMapping2: dict = {}
+            for zwei1 in [0, 1]:
+                for zwei2 in [0, 1]:
+                    for zwei3 in [0, 1]:
+                        alleFractionErgebnisseMapping2[
+                            {
+                                "Gal0Uni1": zwei1,
+                                "mul0div1": None,
+                                "erster0zweiter1": zwei3,
+                            }
+                        ] = (zwei1, zwei2, zwei3)
 
             # hier geht es um die html class Parameter und um Tagging ob Galaxie oder Polygon
             koord2tag, koord2Parameter = {}, {}
