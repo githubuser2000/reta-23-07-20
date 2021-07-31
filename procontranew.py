a9cacd6f9ab03074351a3fddd50dc64403d98ccd
Alexander Kern <alexgit@alexkern.de>
2021-07-31 19:33:13 +0200

    Gegen / pro: Nach Rechenregeln auf Primzahlkreuz

--- procontranew.py
+++ procontranew.py
@@ -8,22 +8,28 @@ from lib4tables import (couldBePrimeNumberPrimzahlkreuz,
 primAmounts = 0
 flag1, flag2 = True, True
 start1, start2 = 1, 5
-for num in range(0, 20):
+list1, list2 = [], []
+for num in range(0, 50):
     if couldBePrimeNumberPrimzahlkreuz(num):
         primAmounts += 1
-    if num > 16:
-        if primCreativity(num) == 1:
+    if primCreativity(num) == 1:
 
-            if couldBePrimeNumberPrimzahlkreuz_fuer_innen(num):
+        if couldBePrimeNumberPrimzahlkreuz_fuer_innen(num):
+            print(str(num) + ": pro innen")
+            list1 += [num]
+            if num > 16:
                 if flag1:
                     pass
-                flag1 = False
-            if couldBePrimeNumberPrimzahlkreuz_fuer_aussen(num):
+            flag1 = False
+        elif not couldBePrimeNumberPrimzahlkreuz_fuer_aussen(num):
+            flag1 = True
+
+        if couldBePrimeNumberPrimzahlkreuz_fuer_aussen(num):
+            print(str(num) + ": pro außen")
+            list1 += [num]
+            if num > 16:
                 if flag2:
                     pass
-                flag2 = False
-        else:
-            if couldBePrimeNumberPrimzahlkreuz_fuer_innen(num):
-                flag1 = True
-            if couldBePrimeNumberPrimzahlkreuz_fuer_aussen(num):
-                flag2 = True
+            flag2 = False
+        elif not couldBePrimeNumberPrimzahlkreuz_fuer_innen(num):
+            flag2 = True
