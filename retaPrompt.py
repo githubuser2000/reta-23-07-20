63d0b55d5764449e0a73daf69cb7adde17cc9da4
Alexander Kern <alexgit@alexkern.de>
2021-08-26 12:32:29 +0200

    ein prompt für reta

--- retaPrompt.py
+++ retaPrompt.py
@@ -1,6 +1,22 @@
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import WordCompleter
+import reta
+from prompt_toolkit import print_formatted_text, prompt
+from prompt_toolkit.completion import NestedCompleter, WordCompleter
+from prompt_toolkit.styles import Style
 
-html_completer = WordCompleter(['<html>', '<body>', '<head>', '<title>'])
-text = prompt('Enter HTML: ', completer=html_completer)
-print('You said: %s' % text)
+# Es gibt einen vi mode in dieser lib
+# html_completer = WordCompleter(["<html>", "<body>", "<head>", "<title>"])
+html_completer = NestedCompleter.from_nested_dict(
+    {
+        "show": {"version": None, "clock": None, "ip": {"interface": {"brief"}}},
+        "exit": None,
+    }
+)
+text = prompt(
+    # print_formatted_text("Enter HTML: ", sep="", end=""), completer=html_completer
+    "Enter HTML: ",
+    completer=html_completer,
+    wrap_lines=False,
+    complete_while_typing=True,
+    vi_mode=True,
+)
+print("You said: %s" % text, end=" ")
