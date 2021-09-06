6f15e1b926f04159a410e7c8c7304bd3b1feddb7
Alexander Kern <alexgit@alexkern.de>
2021-09-07 01:06:17 +0200

    reta Prompt

--- retaPrompt.py
+++ retaPrompt.py
@@ -10,39 +10,6 @@ from prompt_toolkit.completion import Completer, Completion
 from prompt_toolkit.styles import Style
 from word_completerAlx import WordCompleter
 
-
-class MyCustomCompleter(Completer):
-    def get_completions(self, document, complete_event):
-        # yield Completion("abcd", start_position=0)
-        a = (a for a in [0, 1])
-        b = (b for b in [2, 3, 4])
-        # UNSINN DAS MIT GENERATOR ZU MACHEN, DA ES DURCH DIE LIB AM ENDE ZU EINER LISTE WIRD UND DAS LAHMEN WIRD AUCH MIT NUMPY
-        # DAS HEIßT SOGAR INSGESAMT FUNKTIONIERT DAS NICHT SO WIE ICH WILL
-        wortliste = ""
-        for davor in ["-", ""]:
-            for zahl1 in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-                for zahl2 in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ""]:
-                    for zahl3 in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ""]:
-                        for zahl4 in [
-                            "0",
-                            "1",
-                            "2",
-                            "3",
-                            "4",
-                            "5",
-                            "6",
-                            "7",
-                            "8",
-                            "9",
-                            "",
-                        ]:
-                            wortliste += davor + zahl1 + zahl2 + zahl3 + zahl4
-
-        completer = WordCompleter([a for a in ["abcdef", "hijkl"]])
-        for c in completer.get_completions(document, complete_event):
-            yield c
-
-
 retaProgram = reta.Program([sys.argv[0]])
 # print(str(retaProgram.mainParaCmds))
 # print(str(retaProgram.paraDict.keys()))
@@ -78,7 +45,6 @@ html_completer = NestedCompleter.from_nested_dict(
     {
         "show": {"version": None, "clock": None, "ip": {"interface": {"brief"}}},
         "bla": {"version": None, "ip": {"interface": {"brief"}}},
-        "exit": MyCustomCompleter(),
     }
 )
 text = prompt(
