695526c6dc4b9b2178dbfc223f1735ea146812ef
Alexander Kern <alexgit@alexkern.de>
2021-09-07 01:58:25 +0200

    reta Prompt

--- nestedAlx.old2.py
+++ nestedAlx.old2.py
@@ -44,7 +44,9 @@ class NestedCompleter(Completer):
 
     @classmethod
     def from_nested_dict(
-        cls, data: NestedDict, notParameterValues=()
+        cls,
+        data: NestedDict,
+        notParameterValues=(),
     ) -> "NestedCompleter":
         """
         Create a `NestedCompleter`, starting from a nested dictionary data
@@ -103,12 +105,33 @@ class NestedCompleter(Completer):
         # subcompleter.
         if " " in text:
             first_term = text.split()[0]
+            completer = self.options.get(first_term)
+            # completer = self.matchTextAlx(first_term)
+            # print(str(type(completer)))
+
+            # If we have a sub completer, use this for the completions.
+            if completer is not None:
+                remaining_text = text[len(first_term) :].lstrip()
+                move_cursor = len(text) - len(remaining_text) + stripped_len
+
+                new_document = Document(
+                    remaining_text,
+                    cursor_position=document.cursor_position - move_cursor,
+                )
+
+                for c in completer.get_completions(new_document, complete_event):
+                    yield c
+
+        if "=" in text and False:
+            first_term = text.split("=")[0]
             # completer = self.options.get(first_term)
             completer = self.matchTextAlx(first_term)
             # print(str(type(completer)))
 
             # If we have a sub completer, use this for the completions.
             if completer is not None:
+                for notParaVal in self.notParameterValues:
+                    completer.options.pop(notParaVal, None)
                 remaining_text = text[len(first_term) :].lstrip()
                 move_cursor = len(text) - len(remaining_text) + stripped_len
 
