fa42cbcb6cf99bd19fb882e710d0a749b0787c25
Alexander Kern <alexgit@alexkern.de>
2021-09-19 21:26:58 +0200

    reta Prompt , ausführbar - mit kaum Funktion, aber vorbereitet damit es woanders funktioniert

--- nestedAlx.py
+++ nestedAlx.py
@@ -194,13 +194,17 @@ class NestedCompleter(Completer):
                     }
                     completer.lastString = first_term
                     completer.situationsTyp = ComplSitua.komiPara
-            # elif trennzeichen in (",", "="):
-            print(str(self.situationsTyp))
+        elif trennzeichen in (",", "="):
+            # print(str(self.situationsTyp == ComplSitua.spaltenPara))
             if self.situationsTyp == ComplSitua.spaltenPara:
-                print(str(spaltenDict[first_term]))
-                completer.options = {key: None for key in spaltenDict[first_term]}
+                # first_term = first_term[2:]
+                # print(str(first_term) + "JJ")
+                # print(str(spaltenDict) + "JJ")
+                # print(str(spaltenDict[first_term]) + "JJ")
+                completer.options = {key: None for key in spaltenDict[first_term[2:]]}
                 completer.optionsTypes = {
-                    key: ComplSitua.spaltenValPara for key in spaltenDict[first_term]
+                    key: ComplSitua.spaltenValPara
+                    for key in spaltenDict[first_term[2:]]
                 }
                 completer.lastString = first_term
 
