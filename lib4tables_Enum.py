d26d652a1239ed1f1311a7aa5064f5ce97d62eac
Alexander Kern <ak@alexkern.de>
2022-05-14 02:53:18 +0200

    Das erste Reziproke von etwas das nicht 13 oder 15 ist

--- lib4tables_Enum.py
+++ lib4tables_Enum.py
@@ -18,7 +18,8 @@ class ST(Enum):
 
 tableTags = {
     frozenset({ST.sternPolygon, ST.keinParaOdMetaP, ST.universum}): {132, 213, 107, 214,235,240,230,264},
-    frozenset({ST.sternPolygon, ST.keinParaOdMetaP}): {232,233,234,243,249,250,251,252,253,254,255,256,260,261,262,263,265,266,267,268,269,270,271,272,273},
+    frozenset({ST.sternPolygon, ST.keinParaOdMetaP}): {232,233,234,243,249,250,251,252,253,254,255,256,260,261,262,263,265,266,267,268,269,270,271,272},
+    frozenset({ST.keinParaOdMetaP, ST.gleichfoermigesPolygon}): {273},
     frozenset({ST.sternPolygon, ST.galaxie}): {
         0,
         1,
