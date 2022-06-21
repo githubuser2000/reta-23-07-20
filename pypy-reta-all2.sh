92530c3ca08c43a8394c7536ca71c2f85fa90b6d
Alexander Kern <gitPPP@alexkern.de>
2022-06-21 21:44:39 +0200

    html make shellscript deutlich besser

--- pypy-reta-all2.sh
+++ pypy-reta-all2.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-cd /home/alex/myRepos/reta
-cat ~/myRepos/reta/head1.alx
-cat ~/myRepos/reta/religionen.js
-cat ~/myRepos/reta/head2.alx
+trap ctrl_c INT
 if [ "$1" == 'reta' ]; then
-	pypy3 reta -spalten --alles --breite=0 -ausgabe --art=html --onetable --nocolor >  ~/middle.alx && ja=true || ja=false
+	pypy3 ~/myRepos/reta/reta -spalten --alles --breite=0 -ausgabe --art=html --onetable --nocolor >  ~/myRepos/reta/middle1.alx
+    git mv ~/myRepos/reta/middle1.alx ~/myRepos/reta/middle.alx
 fi
-cat ~/middle.alx
-cat ~/myRepos/reta/footer.alx
-cd - > /dev/null
+
+function ctrl_c() {
+    cat ~/myRepos/reta/head1.alx
+    cat ~/myRepos/reta/religionen.js
+    cat ~/myRepos/reta/head2.alx
+    cat ~/myRepos/reta/middle.alx
+    cat ~/myRepos/reta/footer.alx
+}
+
+ctrl_c
