ecfd2f7f442afef77f90585e8236495c28311547
Alexander Kern <alexgit@alexkern.de>
2022-10-27 14:25:44 +0200

    .sh skripte wieder angepasst und mehreres in tabellen dazu: Bestrebungen, Modus, reziprokes

--- pypy-reta-all2.sh
+++ pypy-reta-all2.sh
@@ -4,13 +4,12 @@ function ctrl_c() {
     cat ~/myRepos/reta/head1.alx
     cat ~/myRepos/reta/religionen.js
     cat ~/myRepos/reta/head2.alx
-    cat ~/myRepos/reta/middle.alx
+    cat ~/middle.alx
     cat ~/myRepos/reta/footer.alx
 }
 trap ctrl_c INT
 if [ "$1" == 'reta' ]; then
-	pypy3 ~/myRepos/reta/reta -spalten --alles --breite=0 -ausgabe --art=html --onetable --nocolor >  ~/myRepos/reta/middle1.alx
-    mv ~/myRepos/reta/middle1.alx ~/myRepos/reta/middle.alx
+	pypy3 ~/myRepos/reta/reta -spalten --alles --breite=0 -ausgabe --art=html --onetable --nocolor >  ~/middle1.alx
 fi
 chown -R alex:alex ~/myRepos/reta
 ctrl_c
