eb731048dcb7f268073c762a0ad86355a80bdb82
Alexander Kern <alexgit@alexkern.de>
2023-01-13 19:50:12 +0100

    anfang  mit neuem feature grundstrukturhierarchie für html

---
+++ todel.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env pypy3
+# -*- coding: utf-8 -*-
+import json
+from pprint import pprint
+
+from LibRetaPrompt import wahl15
+
+wahlNeu: dict[str, dict] = {}
+liste: list[str]
+for key, value in wahl15.items():
+    liste = key.split("_")
+    liste = list(filter(None, liste))
+    if len(liste) > 0:
+        thing: dict[str, dict] = {}
+        wahl: dict[str, dict] = wahlNeu
+        for el in reversed(liste):
+            el = el.replace("pro", "/")
+            if thing == {}:
+                thing: dict[dict, str]
+                thing = {el: value}
+            if thing != {}:
+                try:
+                    if type(wahl) is dict and wahl[el] == value:
+                        thing = {el: value}
+                        thing |= {el: thing}
+                        wahl = wahl[el]
+                    else:
+                        thing = {el: thing}
+                        if type(wahl) is dict:
+                            wahl = wahl[el]
+                except:
+                    thing = {el: thing}
+        wahlNeu |= thing
+
+    # wahlNeu[liste[0]] = value
+
+wahlNeu2: dict[str, dict] = {}
+wahlNeu2["15"] = wahlNeu
+
+# pprint(json.dumps(wahlNeu2))
+pprint(wahlNeu2)
