7f90984231a14a895b3f20ca51ce1662c4db97e5
Alexander Kern <alexgit@alexkern.de>
2023-01-14 13:34:38 +0100

    Ich wollte mich eigentlich nur warm programmieren, aber dadurch schließe ich Fehler von vornherein aus: gut, dass ich jetzt ein wenig mehr clean programmiere

--- todel.py
+++ todel.py
@@ -9,72 +9,6 @@ from pprint import pprint
 from LibRetaPrompt import wahl15
 
 
-def merge_dicts(dict1, dict2):
-    for key in dict2:
-        if (
-            key in dict1
-            and isinstance(dict1[key], OrderedDict)
-            and isinstance(dict2[key], OrderedDict)
-        ):
-            merge_dicts(dict1[key], dict2[key])
-        else:
-            if key in dict1:
-                if isinstance(dict2[key], OrderedDict) and not isinstance(
-                    dict1[key], OrderedDict
-                ):
-                    dict1[key] = dict2[key]
-            else:
-                dict1[key] = dict2[key]
-    return dict1
-
-
-wahlNeu: dict[str, dict] = OrderedDict()
-liste: list[str]
-for key, value in wahl15.items():
-    liste = key.split("_")
-    liste = list(filter(None, liste))
-    if len(liste) > 0:
-        thing: dict[str, dict] = OrderedDict()
-
-        # wahl: dict[str, dict] = wahlNeu
-        # adresse1 = []
-        # adresse2 = []
-        for el in reversed(liste):
-            el = el.replace("pro", "/")
-            # adresse1 += [el]
-
-            # vorherSchonDrin = deepcopy(wahlNeu)
-            ## pprint(vorherSchonDrin)
-            # for adresse in reversed(adresse1):
-            #    if vorherSchonDrin is not None and type(vorherSchonDrin) is not str:
-            #        # print(adresse)
-            #        # print(adresse1)
-            #        # print(vorherSchonDrin.keys())
-            #        adresse2 += [adresse]
-            #        try:
-            #            vorherSchonDrin = vorherSchonDrin[adresse]
-            #        except KeyError:
-            #            # print("ERR" + str(adresse))
-            #            vorherSchonDrin = None
-            # if type(vorherSchonDrin) is str:
-            #    vorherSchonDrin = None
-            # print(adresse1)
-            # print(adresse2)
-            # if vorherSchonDrin is not None:
-            # print(vorherSchonDrin.items())
-
-            if thing == OrderedDict():
-                thing: dict[dict, str]
-                thing = {el: OrderedDict({value: None})}
-            else:
-                thing = {el: thing}
-
-        wahlNeu = merge_dicts(thing, wahlNeu)
-        # pprint(wahlNeu)
-
-    # wahlNeu[liste[0]] = value
-
-
 def cmp_before(value):
     value = value[0]
     isNumber: bool = True
@@ -101,10 +35,8 @@ def cmpx(erster, zweiter):
         value2 = int(value2)
         if value1 == value2:
             if "/" in erster[0]:
-                print("A")
                 return 1
             elif "/" in zweiter[0]:
-                print("B")
                 return -1
             else:
                 return 0
@@ -116,6 +48,46 @@ def cmpx(erster, zweiter):
         return -1
     else:
         return value1 > value2
+
+
+def merge_dicts(dict1, dict2):
+    for key in dict2:
+        if (
+            key in dict1
+            and isinstance(dict1[key], OrderedDict)
+            and isinstance(dict2[key], OrderedDict)
+        ):
+            merge_dicts(dict1[key], dict2[key])
+        else:
+            if key in dict1:
+                if isinstance(dict2[key], OrderedDict) and not isinstance(
+                    dict1[key], OrderedDict
+                ):
+                    dict1[key] = dict2[key]
+            else:
+                dict1[key] = dict2[key]
+    return dict1
+
+
+wahlNeu: dict[str, dict] = OrderedDict(sorted({}.items(), key=cmp_to_key(cmpx)))
+
+liste: list[str]
+for key, value in wahl15.items():
+    liste = key.split("_")
+    liste = list(filter(None, liste))
+    if len(liste) > 0:
+        thing: dict[str, dict] = OrderedDict(sorted({}.items(), key=cmp_to_key(cmpx)))
+        for el in reversed(liste):
+            el = el.replace("pro", "/")
+            if thing == OrderedDict(sorted({}.items(), key=cmp_to_key(cmpx))):
+                thing: dict[dict, list]
+                newKeys = value.split(",")
+                newValues = [None] * len(newKeys)
+                thing = {el: OrderedDict(zip(newKeys, newValues))}
+            else:
+                thing = {el: thing}
+
+        wahlNeu = merge_dicts(thing, wahlNeu)
 
 
 wahlNeu2: OrderedDict[str, dict] = OrderedDict()
