bbd6ce32d32b5ff620f5ab32db7d53beafa69b97
Alexander Kern <alexgit@alexkern.de>
2023-01-24 14:16:18 +0100

    bla

--- todel.py
+++ todel.py
@@ -34,4 +34,23 @@ pp(str(len(t)))
 
 a = {a: {a / 2, a / 5, a / 3} for a in range(61)}
 k = {a: primMultiple(a) for a in range(61)}
-pp(k)
+j = {}
+
+for i, a in k.items():
+    if i not in t:
+        j[i] = a[1:]
+
+l = set()
+for a in j.values():
+    for b in a:
+        l |= {*b}
+
+pp(l)
+pp(len(l - t))
+pp("ungefähr Nicht-Primzahlen innerhalb 1 bis 60")
+pp(
+    "Diese setzen sich aus einem Polynom zusammen f(x1,x2,x3) = a + b*2^x1 + c*3^x2 + d*5^x3"
+)
+pp(
+    "Die Potenzen könnte man noch raus-bekommen, zur Vereinfachung, indem man sie ausrechnet, sodas man keinen Exponenten mehr braucht. So sparrt man 3 Zahlen bei so einer Schreibweise."
+)
