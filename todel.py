25124881d92690af78bb61235a038099391fa0c2
Alexander Kern <alexgit@alexkern.de>
2023-01-24 13:08:59 +0100

    15_6 1-36 alles voll

---
+++ todel.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env pypy3
+# -*- coding: utf-8 -*-
+# import numpy as np
+import html
+import json
+import platform
+import re
+from collections import OrderedDict, namedtuple
+from itertools import zip_longest
+from typing import Optional, Union
+
+try:
+    from orderedset import OrderedSet
+except:
+    OrderedSet = set
+
+from center import Primzahlkreuz_pro_contra_strs, retaHilfe
+
+a = {a: {a / 2, a / 5, a / 3} for a in range(61)}
+
+c = {}
+for i, d in a.items():
+    flag = False
+    for b in d:
+        if b == round(b):
+            flag = True
+    if flag:
+        c[i] = b
+
+print(str(c))
