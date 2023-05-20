0d2386807664f5173b064ff7b831226cc36b9e57
Alexander Kern <alexgit@alexkern.de>
2023-05-20 21:18:20 +0200

    refactoring

--- religionen.ts
+++ religionen.ts
@@ -173,9 +173,8 @@ class StarPolygon {
     }
 }
 
-function giveSetOfPolyTypes(numberList: number[]): Set<number>  {
-  var ifDrawPoly: Set<number> = new Set();
-  var Enums: Set<number> | Array<number> = new Set();
+function subFkt1_PolyTpes(Enums1: Array<number> | Set<number> | HTMLCollectionOf<any>): [number[], Set<number>, number[]] {
+  var Enums: Set<number> | Array<number> = new Set(Enums1);
   var abzug: number[] = [];
   if (Enums.has(6) && !Enums.has(0)) abzug.push(0);
   if (Enums.has(6) && !Enums.has(1)) abzug.push(1);
@@ -190,33 +189,27 @@ function giveSetOfPolyTypes(numberList: number[]): Set<number>  {
   if (Enums.has(5) && !Enums.has(3)) abzug.push(3);
   if (Enums.has(5) && !Enums.has(4)) abzug.push(4);
   Enume = new Set([...Enums, ...Enume]);
-  for (var i3 = 0; i3 < abzug.length; i3++) Enume.delete(abzug[i3]);
+  for (var i = 0; i < abzug.length; i++) Enume.delete(abzug[i]);
   Enums  = Array.from(Enume);
+  return [Enums, Enume, abzug];
+}
 
-  for (var i3: number = 2; i3 < spaltenTags.length; i3++) {
-    var enumi: Set<number> = new Set();
-    for (var k3: number = 0; k3 < spaltenTags[i3].length; k3++)
-      for (var l3: number = 0; l3 < Enums.length; l3++)
-        if (spaltenTags[i3][k3] == Enums[l3]) enumi.add(Enums[l3]);
 
-    if (
-      (!enumi.has(0) && !enumi.has(1) && !enumi.has(6)) ||
-      (!enumi.has(3) && !enumi.has(4) && !enumi.has(5)) ||
-      enumi.size == 0
-    ) {
-    /*var spaltenTags2: string;
-    for (var k: number = 0; k < spalten4spaltenTags[i].length; k++) {
-        spaltenTags2 = spalten4spaltenTags[i][k].style.fontSize = "80%";
-        spaltenTags2 = spalten4spaltenTags[i][k].style.opacity = "0.4";
-    }*/
-    } else {
-      for (var k3: number = 0; k3 < spalten4spaltenTags[i3].length; k3++) {
-        ifDrawPoly.add(k3);
-      }
-    }
-  }
+
+function giveSetOfPolyTypes(Enums1: Array<number> | Set<number> | HTMLCollectionOf<any>): Set<number>  {
+  var ifDrawPoly: Set<number> = new Set();
+  var [Enums, Enume, abzug] = subFkt1_PolyTpes(Enums1);
+  subFkt3(Enums, (unimportantVar: number) => {},SubFkt3SubFkt2bPtr, ifDrawPoly);
+  //var enumi: Set<number> = subFkt3(Enums, (unimportantVar: number) => {},SubFkt3SubFkt2bPtr, ifDrawPoly);
   ifDrawPoly.delete(0);
   ifDrawPoly.delete(1);
+  return ifDrawPoly
+}
+
+var SubFkt3SubFkt2bPtr: (i: number, k?: number, ifDrawPoly?:Set<number>,spalten4spaltenTags?: any) => void | Set<number> = function SubFkt3SubFkt2b(i: number, k: number=0, ifDrawPoly: Set<number> = new Set(), spalten4spaltenTags: any = {}): Set<number> {
+  for (var k: number = 0; k < spalten4spaltenTags[i].length; k++) {
+    ifDrawPoly.add(k);
+  }
   return ifDrawPoly
 }
 
@@ -665,8 +658,8 @@ for (i = 0; i < tdClasses1.length; i++)
   var gfPolygon: gleichfPolygon;
   var polyg1: string;
   var polyg2: string;
-  var ifDrawSpoly: Set<number> = new Set();
-  var ifDrawgfPoly: Set<number> = new Set();
+  var ifDrawSpoly: Set<number>;
+  var ifDrawgfPoly: Set<number>;
   let pSize: number = 120;
   var i2: number;
   var ueberschrift: string;
@@ -757,70 +750,10 @@ function makeMapsOfHeadLCheckB(p1: string, p2: string | null, num: string | numb
 }
 
 function disEnAbleChks(Enums1: Array<number> | Set<number> | HTMLCollectionOf<any>) {
-  var Enums: Set<number> | Array<number> = new Set(Enums1);
-  var abzug: number[] = [];
-  if (Enums.has(6) && !Enums.has(0)) abzug.push(0);
-  if (Enums.has(6) && !Enums.has(1)) abzug.push(1);
-  if (Enums.has(1) && !Enums.has(0)) abzug.push(0);
-  if (Enums.has(1) && !Enums.has(6)) abzug.push(6);
-  if (Enums.has(0) && !Enums.has(1)) abzug.push(1);
-  if (Enums.has(0) && !Enums.has(6)) abzug.push(6);
-  if (Enums.has(3) && !Enums.has(4)) abzug.push(4);
-  if (Enums.has(3) && !Enums.has(5)) abzug.push(5);
-  if (Enums.has(4) && !Enums.has(3)) abzug.push(3);
-  if (Enums.has(4) && !Enums.has(5)) abzug.push(5);
-  if (Enums.has(5) && !Enums.has(3)) abzug.push(3);
-  if (Enums.has(5) && !Enums.has(4)) abzug.push(4);
-  Enume = new Set([...Enums, ...Enume]);
-  for (var i = 0; i < abzug.length; i++) Enume.delete(abzug[i]);
-  Enums  = Array.from(Enume);
+  var [Enums, Enume, abzug] = subFkt1_PolyTpes(Enums1);
+  var enumi: Set<number> = subFkt3(Enums, SubFkt3SubFkt1Ptr, SubFkt3SubFkt2Ptr, new Set());
 
-  for (var i: number = 0; i < chks2.length; i++) {
-    var enumi: Set<number> = new Set();
-    for (var k: number = 0; k < chks2[i].length; k++)
-      for (var l: number = 0; l < Enums.length; l++)
-        if (chks2[i][k] == Enums[l].toString()) enumi.add(Enums[l]);
-    if (
-      (!enumi.has(0) && !enumi.has(1) && !enumi.has(6)) ||
-      (!enumi.has(3) && !enumi.has(4) && !enumi.has(5)) ||
-      enumi.size == 0
-    ) {
-      chks1[i].disabled = true;
-      chks1[i].style.fontSize = tdStyleFontSizeKl;
-      chks1[i].style.color = tdStyleColorKl;
-      chks1[i].style.whiteSpace = tdStyleWhiteSpace;
-    } else {
-      chks1[i].disabled = false;
-      chks1[i].style.fontSize = tdStyleFontSize;
-      chks1[i].style.color = "";
-      chks1[i].style.whiteSpace = tdStyleWhiteSpace;
-    }
-  }
-
-  for (var i: number = 2; i < spaltenTags.length; i++) {
-    var enumi: Set<number> = new Set();
-    for (var k: number = 0; k < spaltenTags[i].length; k++)
-      for (var l: number = 0; l < Enums.length; l++)
-        if (spaltenTags[i][k] == Enums[l]) enumi.add(Enums[l]);
-
-    if (
-      (!enumi.has(0) && !enumi.has(1) && !enumi.has(6)) ||
-      (!enumi.has(3) && !enumi.has(4) && !enumi.has(5)) ||
-      enumi.size == 0
-    ) {
-      var spaltenTags2: string;
-      for (var k: number = 0; k < spalten4spaltenTags[i].length; k++) {
-        spaltenTags2 = spalten4spaltenTags[i][k].style.fontSize = "80%";
-        spaltenTags2 = spalten4spaltenTags[i][k].style.opacity = "0.4";
-      }
-    } else {
-      for (var k: number = 0; k < spalten4spaltenTags[i].length; k++) {
-        spaltenTags2 = spalten4spaltenTags[i][k].style.fontSize = "100%";
-        spaltenTags2 = spalten4spaltenTags[i][k].style.opacity = "1.0";
-      }
-    }
-  }
-  Achks = document.getElementsByClassName("chksA");
+  var Achks: HTMLCollectionOf<HTMLInputElement> = document.getElementsByClassName("chksA") as HTMLCollectionOf<HTMLInputElement>;
   var Bchks: HTMLCollectionOf<HTMLInputElement>;
   for (var i: number = 0; i < Achks.length; i++) {
     Bchks = Achks[i]
@@ -887,6 +820,39 @@ const primZahlen: number[] = [
   919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019,
   1021,
 ];
+
+function subFkt3(Enums: number[], SubFkt3SubFkt2Var: (i3: number, k3?: number, ifDrawPoly?:Set<number>, spalten4spaltenTags?: any) => void | Set<number>, SubFkt3SubFkt1Var: (i3: number, k3?: number, ifDrawPoly?:Set<number>, spalten4spaltenTags?: any) => void | Set<number>, ifDrawPoly: Set<number>): Set<number> {
+  var enumi: Set<number> = new Set();
+  for (var i: number = 0; i < chks2.length; i++) {
+    enumi = new Set();
+    for (var k: number = 0; k < chks2[i].length; k++)
+      for (var l: number = 0; l < Enums.length; l++)
+        if (chks2[i][k] == Enums[l].toString())
+          enumi.add(Enums[l]);
+    if ((!enumi.has(0) && !enumi.has(1) && !enumi.has(6)) ||
+      (!enumi.has(3) && !enumi.has(4) && !enumi.has(5)) ||
+      enumi.size == 0) {
+      SubFkt3SubFkt1Var(i,k,ifDrawPoly,spalten4spaltenTags);
+    } else {
+      SubFkt3SubFkt2Var(i,k,ifDrawPoly,spalten4spaltenTags);
+    }
+  }
+  return enumi;
+}
+
+var SubFkt3SubFkt2Ptr: (i: number, k?: number, ifDrawPoly?:Set<number>, spalten4spaltenTags?: any) => void | Set<number> = function SubFkt3SubFkt2(i: number,k: number=0, ifDrawPoly: Set<number> = new Set(), spalten4spaltenTags: any = {}): void {
+  chks1[i].disabled = false;
+  chks1[i].style.fontSize = tdStyleFontSize;
+  chks1[i].style.color = "";
+  chks1[i].style.whiteSpace = tdStyleWhiteSpace;
+}
+
+var SubFkt3SubFkt1Ptr: (i: number, k?: number, ifDrawPoly?:Set<number>, spalten4spaltenTags?: any) => void | Set<number> = function SubFkt3SubFkt1(i: number,k: number=0, ifDrawPoly: Set<number> = new Set(), spalten4spaltenTags: any = {}): void {
+  chks1[i].disabled = true;
+  chks1[i].style.fontSize = tdStyleFontSizeKl;
+  chks1[i].style.color = tdStyleColorKl;
+  chks1[i].style.whiteSpace = tdStyleWhiteSpace;
+}
 
 function makeSpacesOutOf_(text: string): string {
   //if (text.length == 10) if (text == "Wichtigste") return "<b>Wichtigste</b>";
